0eb8f1e40b6b9fcd9f46897b88c6af8be2df9c8b
GurtyGurt98 <austinphillipssps@gmail.com>
2023-08-24 12:40:33 -0400

    t

--- SQL_API/Models/FestivalModel.cs
+++ SQL_API/Models/FestivalModel.cs
@@ -8,12 +8,12 @@ namespace SQL_API.Models
 {
     public class FestivalModel
     {
-        public string Name { get; set; }
-        public int IDFestival { get; set; }
-        public DateOnly Start { get; set; }
-        public DateOnly End { get; set; }
-        public string Location { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = "Food and Wine 1955";
+        public int IDFestival { get; set; } = 999999;
+        public DateOnly Start { get; set; } = new DateOnly(1995, 1, 1);
+        public DateOnly End { get; set; } = new DateOnly(1995, 12, 31);
+        public string Location { get; set; } = "Epcot";
+        public string Description { get; set; } = "This is a default festival for the Festival Model";
         public FestivalModel() { }
         public FestivalModel(string name, int id, DateOnly start, DateOnly end, string location, string description)
         {
