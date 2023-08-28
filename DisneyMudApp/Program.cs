18a62f84623ca32aea9c1e70d4a189cceb4652f3
Gurtygurt98 <austinphillipssps@gmail.com>
2023-08-28 17:26:59 -0400

    d

--- DisneyMudApp/Program.cs
+++ DisneyMudApp/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddTransient<ISQLDataAccess, SQLDataAccess>();
 builder.Services.AddTransient<IFoodData, FoodData>();
+builder.Services.AddTransient<IFestivalData, FestivalData>();
 builder.Services.AddMudServices();
 
 var app = builder.Build();
